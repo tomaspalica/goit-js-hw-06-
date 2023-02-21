623326e664dfec371131bc26e7d38717b405917e
tomaspalica <tomasss.palica@gmail.com>
2023-02-21 15:01:59 +0100

    added changes to task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,16 @@
 const validationInput = document.querySelector("#validation-input");
-const validationInputLength = Number(validationInput.getAttribute("data-length"))
+const validationInputLength = Number(
+  validationInput.getAttribute("data-length")
+);
 
-const colorChange = () => {if(validationInput.value.length === validationInputLength){
-validationInput.classList.add("valid")
-} else {
-  validationInput.classList.add("invalid")
-}
-}
-validationInput.addEventListener("blur", colorChange)
-
-
-
+const colorChange = () => {
+  if (validationInput.value.length === validationInputLength) {
+    validationInput.classList.add("valid");
+  } else if (validationInput.value.length === 0) {
+    validationInput.classList.remove("valid");
+    validationInput.classList.remove("invalid");
+  } else {
+    validationInput.classList.add("invalid");
+  }
+};
+validationInput.addEventListener("blur", colorChange);
