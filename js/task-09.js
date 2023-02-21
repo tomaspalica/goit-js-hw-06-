1fb336e500998977f9c4d7ffb076bfef396af008
tomaspalica <tomasss.palica@gmail.com>
2023-02-21 16:49:12 +0100

    finished task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+const colorName = document.querySelector(".color")
+const colorChange = document.querySelector(".change-color")
+const body = document.querySelector("body")
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+const bodyColor = () => {
+  
+body.style.backgroundColor = getRandomHexColor()
+colorName.textContent = getRandomHexColor()
 }
+
+colorChange.addEventListener("click", bodyColor)
+
+
+
+
+
+
