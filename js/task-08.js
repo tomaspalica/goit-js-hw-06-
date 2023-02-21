343c53298087602a603c540afee09910fb8db528
tomaspalica <tomasss.palica@gmail.com>
2023-02-21 16:20:12 +0100

    finished task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+const submitHendler = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+  }
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", submitHendler);
